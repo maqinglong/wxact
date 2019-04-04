8c48935652d98f0998407796162cc7e6d7a5cdb3
maqinglong <36579977@qq.com>
2019-04-04 14:13:28 +0800

    日志输出

--- src/main/java/com/ccb/wxact/modules/usermanage/controller/WxAct.java
+++ src/main/java/com/ccb/wxact/modules/usermanage/controller/WxAct.java
@@ -1,5 +1,7 @@
 package com.ccb.wxact.modules.usermanage.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import com.ccb.wxact.modules.usermanage.model.ActUserKey;
 @Controller
 @RequestMapping(value="/waqitest")
 public class WxAct {
+	private Logger logger = LoggerFactory.getLogger(WxAct.class.getName());
 	@Autowired
 	ActUserMapper actuserMapper;
 	@RequestMapping("/hello11")
@@ -21,6 +24,7 @@ public class WxAct {
 		ActUserKey actUserKey = new ActUserKey();
 		actUserKey.setId(1l);
 		actUserKey.setUserNo("1");
+		logger.info("处理开始！");
 		ActUser actUser = actuserMapper.selectByPrimaryKey(actUserKey);
 		return JSONObject.toJSONString(actUser) ;
 	}
